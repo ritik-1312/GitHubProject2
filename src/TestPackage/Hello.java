763bca5ff27243448142d9cd79786af86386d21c
ritik-1312 <hritikgawali@gmail.com>
2023-11-09 23:00:01 +0530

    2nd commit added Hello.java file

--- src/TestPackage/Hello.java
+++ src/TestPackage/Hello.java
@@ -1,10 +1,10 @@
 package TestPackage;
 
-public class Test {
+public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		System.out.println("Hello Ritik");
 	}
 
 }
